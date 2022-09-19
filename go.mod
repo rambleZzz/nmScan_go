4f24c4565abc96a70cc5b9307f3d9feaa93c65f2
rambleZzz <31800843+rambleZzz@users.noreply.github.com>
2022-09-19 13:44:43 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,40 @@
+module github.com/rambleZzz/nmScan_go
+
+go 1.17
+
+require (
+	github.com/Ullaakut/nmap/v2 v2.2.1
+	github.com/goinggo/mapstructure v0.0.0-20140717182941-194205d9b4a9
+	github.com/spf13/viper v1.13.0
+	github.com/urfave/cli/v2 v2.16.3
+	gorm.io/driver/sqlite v1.3.6
+	gorm.io/gorm v1.23.9
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fsnotify/fsnotify v1.5.4 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"github.com/rambleZzz/nmScan_go/core"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
+
+func main() {
+
+	var (
+		model   string
+		file    string
+		port    string
+		threads int
+		oSqlite bool
+		oStxt   bool
+	)
+
+	var app = &cli.App{
+		Name:    "nmScan_go",
+		Version: "v1.0",
+		Usage:   "一款基于GO语言编写的支持调用fofa搜索结果、masscan json格式扫描结果、\n直接调用ip和ip:port格式4种方式进行nmap快速扫描，多种格式输出的主机端口信息的收集工具",
+		UsageText: "nmScan -f ip.txt -m ip	(ip格式扫描模式，使用内置的常见端口扫描，并输出结果至sqlite)\n" +
+			"nmScan -f ip.txt -m ip -p 22,445,3306,6379,8001-8005	(ip格式扫描模式，并指定端口扫描，并输出结果至sqlite)\n" +
+			"nmScan -f ip.txt -m iport	(ip:port格式扫描模式，并输出结果至sqlite)\n" +
+			"nmScan -f ip.txt -m fofa	(从fofa搜索ip开放端口扫描模式,fofa模式支持ip、ip/24、ip/25等格式内容，并输出结果至sqlite)\n" +
+			"nmScan -f ip.json -m masjson	(解析masscan json扫描结果后的扫描模式，并输出结果至sqlite)\n" +
+			"nmScan -f ip.txt -m fofa -oT	(输出结果至sqlite和txt) \n" +
+			"\n" +
+			"-oS表示输出到sqlite，此参数默认为true,不加也会输出,如果-oS false表示不输出至sqlite\n" +
+			"-oT表示输出到txt，此参数默认为false,如果需要导出到文本需添加此参数 \n" +
+			"f和m参数为必填项，其他参数为可选项，更多参数请参考GLOBAL OPTIONS",
+		Action: core.TaskNew,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "file",
+				Aliases:     []string{"f"},
+				Usage:       "从txt或json文件中读取内容 ip.txt/ip.json",
+				Destination: &file,
+			},
+			&cli.StringFlag{
+				Name:        "model",
+				Aliases:     []string{"m"},
+				Usage:       "model参数决定目标解析及扫描方式(四选一) ip/iport/masjson/fofa",
+				Destination: &model,
+			},
+			&cli.StringFlag{
+				Name:        "port",
+				Aliases:     []string{"p"},
+				Usage:       "指定扫描的端口 如22,80,3306,8081-8090",
+				Destination: &port,
+			},
+			&cli.IntFlag{
+				Name:        "threads",
+				Aliases:     []string{"t"},
+				Value:       100,
+				Usage:       "扫描线程数 ",
+				Destination: &threads,
+			},
+			&cli.BoolFlag{
+				Name:        "oSqlite",
+				Aliases:     []string{"oS"},
+				Value:       true,
+				Usage:       "是否输出到sqlite",
+				Destination: &oSqlite,
+			},
+			&cli.BoolFlag{
+				Name:        "oTxt",
+				Aliases:     []string{"oT"},
+				Value:       false,
+				Usage:       "是否输出到txt ",
+				Destination: &oStxt,
+			},
+			// 省略剩余的 StringFlag...
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
